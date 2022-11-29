827406a8bc519f37da1d61ee100129d13ba84d06
borv <bruno-reis.alves@gft.com>
2022-11-29 13:17:28 -0300

    Load balancing

--- photoapp-users/src/main/java/com/photoapp/api/users/controller/UserController.java
+++ photoapp-users/src/main/java/com/photoapp/api/users/controller/UserController.java
@@ -1,16 +1,22 @@
 package com.photoapp.api.users.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping("v1/users")
+@AllArgsConstructor
 public class UserController {
-	
+
+	private final Environment env;
+
 	@GetMapping("status/check")
 	public String status() {
-		return "Ok";
+		return "Ok, on port: " + env.getProperty("local.server.port");
 	}
 
 }
