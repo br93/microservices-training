d4ccb82c24bc1a81a95fa1a1dcb4f668689bb26d
br93 <reisalvesb@gmail.com>
2022-11-29 23:47:55 -0300

    Update Spring Security and API Gateway

--- photoapp-users/src/main/java/com/photoapp/api/users/security/WebSecurity.java
+++ photoapp-users/src/main/java/com/photoapp/api/users/security/WebSecurity.java
@@ -2,17 +2,23 @@ package com.photoapp.api.users.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lombok.AllArgsConstructor;
+
 @Configuration
+@AllArgsConstructor
 public class WebSecurity {
 
+	private final Environment environment;
+	
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((authorize) -> {
-			authorize.anyRequest().permitAll();
+		http.authorizeRequests((authorize) -> {
+			authorize.antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));
 		});
 
 		http.csrf(csrf -> csrf.disable());
