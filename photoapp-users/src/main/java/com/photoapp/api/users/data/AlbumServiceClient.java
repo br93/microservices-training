63003d9337122dfc1bfe4f8d56d7f17c1d1f8a98
borv <bruno-reis.alves@gft.com>
2023-01-09 16:17:29 -0300

    Resilience4J Circuit Breaker

--- photoapp-users/src/main/java/com/photoapp/api/users/data/AlbumServiceClient.java
+++ photoapp-users/src/main/java/com/photoapp/api/users/data/AlbumServiceClient.java
@@ -1,5 +1,6 @@
 package com.photoapp.api.users.data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,9 +9,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.photoapp.api.users.model.AlbumResponseModel;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @FeignClient(name="albums-ms")
 public interface AlbumServiceClient {
 
 	@GetMapping("/v1/users/{id}/albums")
+	@CircuitBreaker(name = "albums-ms", fallbackMethod = "getAlbumsFallback")
 	public List<AlbumResponseModel> getAlbums(@PathVariable String id);
+	
+	default List<AlbumResponseModel> getAlbumsFallback(String id, Throwable exception){
+		return new ArrayList<>();
+	}
 }
