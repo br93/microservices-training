6b36d4e014b861a9ef5eec7764cf27296f47d281
borv <bruno-reis.alves@gft.com>
2023-01-10 13:53:40 -0300

    Resilience4J Retry

--- photoapp-users/src/main/java/com/photoapp/api/users/data/AlbumServiceClient.java
+++ photoapp-users/src/main/java/com/photoapp/api/users/data/AlbumServiceClient.java
@@ -10,11 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.photoapp.api.users.model.AlbumResponseModel;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @FeignClient(name="albums-ms")
 public interface AlbumServiceClient {
 
 	@GetMapping("/v1/users/{id}/albums")
+	@Retry(name = "albums-ms")
 	@CircuitBreaker(name = "albums-ms", fallbackMethod = "getAlbumsFallback")
 	public List<AlbumResponseModel> getAlbums(@PathVariable String id);
 	
