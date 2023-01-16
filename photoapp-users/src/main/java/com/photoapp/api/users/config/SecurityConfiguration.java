c9cf5f48bb8d48dc6abc56a1c00964412a9ae04e
br93 <reisalvesb@gmail.com>
2023-01-15 21:58:17 -0300

    Refactoring SecurityFilterChain

--- photoapp-users/src/main/java/com/photoapp/api/users/config/SecurityConfiguration.java
+++ photoapp-users/src/main/java/com/photoapp/api/users/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -24,17 +25,16 @@ public class SecurityConfiguration {
 	
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		
 		http
-			.csrf().disable()
-			.authorizeHttpRequests()
+			.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(auth -> auth
 				.requestMatchers(environment.getProperty("api.h2.console.url.path")).permitAll()
 				.requestMatchers(environment.getProperty("api.actuator.url.path")).permitAll()
 				.requestMatchers(HttpMethod.POST, "/v1/users").permitAll()
 				.requestMatchers(HttpMethod.POST, "/v1/auth").permitAll()
-				.anyRequest().authenticated()
-			.and()
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
+				.anyRequest().authenticated())
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authenticationProvider(authProvider)
 			.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 		
