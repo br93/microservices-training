5278c17e977130b49a624e45398580fb15f5dd8d
borv <bruno-reis.alves@gft.com>
2023-01-16 22:52:45 -0300

    Custom Health Check

---
+++ photoapp-users/health/HealthCheckConfigServer.java
@@ -0,0 +1,25 @@
+package com.photoapp.api;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class HealthCheckConfigServer {
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+		
+		var client = HttpClient.newHttpClient();
+		var request = HttpRequest.newBuilder()
+				.uri(URI.create("http://localhost:8888/actuator/health"))
+				.build();
+		
+		var response = client.send(request, BodyHandlers.ofString());
+		
+		if (response.statusCode() != 200 || !response.body().contains("UP")) {
+			throw new RuntimeException("Unhealthy");
+		}
+		
+	}
+}
